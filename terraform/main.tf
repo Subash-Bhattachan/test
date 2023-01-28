c4071d3ceb5c8c43d93f6bc69564d5e59de5d165
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2023-01-28 18:23:23 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "TerraformDemo"
+    container_name       = "tfstatefile"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+module "RG" {
+  source   = "./modules/RG" #A
+  rgname   = var.rgname     #B
+  location = var.location
+}
